829b3ab85cf1912ee9c3f46f695d192546dbc13f
Parselable <605113281@qq.com>
2017-03-08 11:55:53 +0800

    no message

--- BDemo/app/src/main/java/com/sola/bdemo/MainActivity.java
+++ BDemo/app/src/main/java/com/sola/bdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次写的东西
+        //第二次
     }
 }
