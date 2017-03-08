2550a10c1d5e8ecea4516b97edac443201d3bcb7
Parselable <605113281@qq.com>
2017-03-08 11:54:11 +0800

    diiici

---
+++ BDemo/app/src/main/java/com/sola/bdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sola.bdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次写的东西
+    }
+}
